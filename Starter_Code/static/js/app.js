71a3e8f2b85a360befa869289bd471e764167f18
daluongo <desiree.luongo@gmail.com>
2020-01-22 16:56:40 -0500

    completed JS

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -5,7 +5,7 @@ function buildMetadata(sample) {
         var resultArray = meta_data.filter(sampleObj => sampleObj.id == sample);
         var result = resultArray[0];
 
-        var panel = d3.select("sample-metadata");
+        var panel = d3.select("#sample-metadata");
 
         panel.html("");
 
@@ -14,32 +14,39 @@ function buildMetadata(sample) {
             
             console.log(key, value);
         });
+    // Calling Gauge Chart function
+        buildGauge(results.wfreq);
     });
 }
 
 function buildCharts(sample) {
     d3.json("data/samples.json").then((data) => {
-        var sample_values = data.sample_values;
-        var otu_ids = data.otu_ids;
-        var otu_labels = data.otu_labels;
+        var samples = data.samples;
+        var result_list = samples.filter(sampleObj => sampleObj.id == sample);
+        var results = result_list[0];
+        
+        var sample_values = results.sample_values;
+        var otu_ids = results.otu_ids;
+        var otu_labels = results.otu_labels;
 
-        console.log(otu_ids, otu_labels, sample_values);
+        // console.log(otu_ids, otu_labels, sample_values);
 
         // Bar chart
+        var y_value = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+
         var bar_data = [
             {
-            x: sample_values.slice(0,10).reverse(),
-            y: otu_ids.slice(0,10).reverse(),
-            text: otu_labels.slice(010).reverse(),
-            type: 'bar',
-            orientation: 'h'
+            x: sample_values.slice(0, 10).reverse(),
+            y: y_value,
+            text: otu_labels.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: "h",
             }
         ];
         var bar_layout = {
-            title: "Top 10 Bacteria Cultures Found",
-            xaxis: { title: "Sample Values"},
-            yaxis: { title: "OTU ID"}
+            title: "Top 10 Bacteria Cultures Found"
         };
+
         Plotly.newPlot("bar", bar_data, bar_layout);
 
         // Bubble Chart
@@ -48,37 +55,40 @@ function buildCharts(sample) {
                 x: otu_ids,
                 y: sample_values,
                 text: otu_labels,
+                mode: "markers",
                 marker: {
                     size: sample_values,
                     color: otu_ids,
                     colorscale: "Earth"
-                },
-                mode: "markers"
+                }
             } 
         ]; 
         var bubble_layout = {
             title: "Bacteria Samples",
-            showlegend: false,
+            margin: { t: 0},
+            // showlegend: false,
             hovermode: "closest",
             xaxis: {title: "OTU ID"},
-            margin: { t: 0}
+            margin: { t: 30}
         };
+
         Plotly.newPlot("bubble", bubble_data, bubble_layout);
     });
-  
 }
 
 // This function is called when a dropdown menu item is selected
 function init() {
+    
     var dropdownMenu = d3.select("#selDataset");
 
-    d3.json("samples.json").then((data) => {
+    d3.json("data/samples.json").then((data) => {
         var sampleNames = data.names;
+        
         sampleNames.forEach((sample) => {
             dropdownMenu
-            .append("option")
-            .text(sample)
-            .property("value", sample);
+                .append("option")
+                .text(sample)
+                .property("value", sample);
         });
 
         // Using first sample data to create initial charts
